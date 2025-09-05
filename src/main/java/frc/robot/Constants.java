e89d30e1de3e2ed7c850cd60cc32b68057dfa30d
ariakrishnamoorthy <aria.r.krishnamoorthy@gmail.com>
2025-09-04 20:14:33 -0700

    adding vision

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,7 +1,18 @@
 package frc.robot;
 
 import static edu.wpi.first.units.Units.*;
+
+import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N3;
+import edu.wpi.first.math.util.Units;
+import frc.robot.RobotContainer.RobotName;
 import frc.robot.generated.TunerConstants;
 
 public class Constants {
@@ -24,4 +35,52 @@ public class Constants {
         public static final double MAX_ROTATION_SPEED = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
 
     }
+    public static class VisionConstants {
+    public static final String CAMERA1_NAME = "beam_camera";  
+    public static final String CAMERA2_NAME = "swerve_camera";  
+
+    public static final double ROBOT_TO_CAM1_X = RobotContainer.name == RobotName.CERIDWEN ? Units.inchesToMeters(-2) : 0.31115 ; // in meters from center of robot 
+    public static final double ROBOT_TO_CAM1_Y = RobotContainer.name == RobotName.CERIDWEN ? Units.inchesToMeters(-1) : -0.0508; // in meters from center of robot 
+    public static final double ROBOT_TO_CAM1_Z = RobotContainer.name == RobotName.CERIDWEN ? Units.inchesToMeters(17) : 0.1397; // in meters from the floor?
+    
+    public static final double ROBOT_TO_CAM2_X = RobotContainer.name == RobotName.CERIDWEN ? Units.inchesToMeters(13-4.75) : 0 ; // in meters from center of robot 
+    public static final double ROBOT_TO_CAM2_Y = RobotContainer.name == RobotName.CERIDWEN ? Units.inchesToMeters(13-3.125) : 0; // in meters from center of robot 
+    public static final double ROBOT_TO_CAM2_Z = RobotContainer.name == RobotName.CERIDWEN ? Units.inchesToMeters(7.5) : 0; // in meters from the floor?
+    
+
+    // default vision standard deviation
+    public static final Matrix<N3, N1> kSingleTagStdDevs = VecBuilder.fill(6, 6, 4);
+    public static final Matrix<N3, N1> kMultiTagStdDevs = VecBuilder.fill(0.5, 0.5, 0.3);
+
+    public static final double MAX_POSE_AMBIGUITY = 0.2;
+    public static final double MAX_VISION_POSE_DISTANCE = 1;
+    public static final double MAX_VISION_POSE_Z = 0.1;
+    public static final double MAX_VISION_POSE_ROLL = 0.05; // in radians
+    public static final double MAX_VISION_POSE_PITCH = 0.05; // in radians
+    public static final double VISION_DISTANCE_DISCARD = 10; 
+
+    public static final double VISION_ALIGNMENT_DISCARD = Units.inchesToMeters(1); // in meters
+
+
+    public static final Translation2d ROBOT_TO_CAM1 = 
+      new Translation2d(ROBOT_TO_CAM1_X, ROBOT_TO_CAM1_Y); // in meters from center of robot to 2x4 camera mount
+
+    public static final Translation2d ROBOT_TO_CAM2 = 
+      new Translation2d(ROBOT_TO_CAM2_X, ROBOT_TO_CAM2_Y); // in meters from center of robot to 2x4 camera mount
+
+    public static final Transform3d ROBOT_TO_CAM1_3D = 
+      new Transform3d(new Translation3d(ROBOT_TO_CAM1_X, ROBOT_TO_CAM1_Y, ROBOT_TO_CAM1_Z), new Rotation3d(0,0,0)); // in meters from center of robot to 2x4 camera mount
+    
+    public static final Transform3d ROBOT_TO_CAM2_3D = 
+      new Transform3d(new Translation3d(ROBOT_TO_CAM2_X, ROBOT_TO_CAM2_Y, ROBOT_TO_CAM2_Z), new Rotation3d(0,0,-Math.PI/9)); // in meters from center of robot to 2x4 camera mount
+  
+
+    public static final double CAMERA_HEIGHT = 0.0951738; // in meters from floor to camera center
+    public static final double CAMERA_PITCH = 0; // in radians, bogus
+    public static final double TARGET_HEIGHT = 0.3048; // in meters to the middle of the apriltag on reef
+    public static final double TARGET_PITCH = 0;
+
+    public static final double MAX_AVG_DIST_BETWEEN_LAST_EST_POSES = 0.3; // in meters 
+    public static final int NUM_LAST_EST_POSES = 3;
+  }
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,10 +6,20 @@ package frc.robot;
 
 import static edu.wpi.first.units.Units.*;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.photonvision.EstimatedRobotPose;
+
+import com.ctre.phoenix6.Utils;
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.swerve.SwerveRequest;
 
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -17,10 +27,19 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
-
+import frc.robot.subsystems.LocalizationCamera;
+import frc.robot.subsystems.VisionSubsystem;
 import frc.robot.Constants.DrivetrainConstants;
+import frc.robot.Constants.VisionConstants;
 
 public class RobotContainer {
+    public enum RobotName {
+        CERIDWEN,
+        DYNAMENE
+    }
+    public static RobotName name = RobotName.CERIDWEN;
+
+
 
     public record JoystickVals(double x, double y) {}
     
@@ -38,6 +57,9 @@ public class RobotContainer {
     private final CommandXboxController joystick = new CommandXboxController(0);
 
     public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
+
+     private final VisionSubsystem m_vision = new VisionSubsystem();
+    private final Field2d m_actualField = new Field2d();
 
     public RobotContainer() {
         configureBindings();
@@ -91,9 +113,66 @@ public class RobotContainer {
         joystick.leftBumper().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
 
         drivetrain.registerTelemetry(logger::telemeterize);
+        CameraServer.startAutomaticCapture();
     }
+
 
     public Command getAutonomousCommand() {
         return Commands.print("No autonomous command configured");
     }
+    public void updatePoseEst() {
+    List<LocalizationCamera> cameras = m_vision.getLocalizationCameras();
+
+    for (LocalizationCamera cam : cameras){
+      updatePoseEst(cam);
+    }
+    
+    m_actualField.setRobotPose(drivetrain.getState().Pose);
+  }
+
+    public void updatePoseEst(LocalizationCamera camera){
+      EstimatedRobotPose robotPose = camera.getRobotPose();
+      
+      if (robotPose != null && camera.getTargetFound() && camera.getIsNewResult()) {
+        Pose3d estPose3d = robotPose.estimatedPose; // estimated robot pose of vision
+        Pose2d estPose2d = estPose3d.toPose2d();
+
+        // check if new estimated pose and previous pose are less than 2 meters apart (fused poseEst)
+        double distance = estPose2d.getTranslation().getDistance(drivetrain.getState().Pose.getTranslation());
+
+        SmartDashboard.putNumber("fusedVision/" + camera.getCameraName() + "/distanceBetweenVisionAndActualPose", distance);
+        if (distance < VisionConstants.MAX_VISION_POSE_DISTANCE || !camera.isEstPoseJumpy()) {
+          drivetrain.setVisionMeasurementStdDevs(camera.getCurrentStdDevs());
+          
+          // sample drivetrain fusedPose before updating
+          Optional<Pose2d> samplePose = drivetrain.samplePoseAt(Utils.fpgaToCurrentTime(robotPose.timestampSeconds));
+
+          if (samplePose.isPresent()){
+            SmartDashboard.putNumberArray("fusedVision/" + camera.getCameraName() + "/samplePose",  new double [] {
+              samplePose.get().getX(), samplePose.get().getY(), samplePose.get().getRotation().getRadians()});
+          }
+          
+          SmartDashboard.putNumberArray("fusedVision/" + camera.getCameraName() + "/drivetrainBeforeUpdate", new double [] {
+          drivetrain.getState().Pose.getX(), drivetrain.getState().Pose.getY(), drivetrain.getState().Pose.getRotation().getRadians()});
+
+
+          drivetrain.addVisionMeasurement(estPose2d, Utils.fpgaToCurrentTime(robotPose.timestampSeconds));
+          camera.updateField(estPose2d);
+
+          // sample drivetrain fusedPose after updating
+          SmartDashboard.putNumberArray("fusedVision/" + camera.getCameraName() + "/drivetrainAfterUpdate", new double [] {
+            drivetrain.getState().Pose.getX(), drivetrain.getState().Pose.getY(), drivetrain.getState().Pose.getRotation().getRadians()});
+            
+          SmartDashboard.putNumberArray("fusedVision/" + camera.getCameraName() + "/visionPose2dFiltered" + camera.getCameraName(), new double[] {estPose2d.getX(), estPose2d.getY(), estPose2d.getRotation().getRadians()});
+      }
+
+      SmartDashboard.putNumberArray("fusedVision/" + camera.getCameraName() + "/visionPose3D", new double[] {
+        estPose3d.getX(),
+        estPose3d.getY(),
+        estPose3d.getZ(),
+        estPose3d.getRotation().toRotation2d().getRadians()
+      }); // post vision 3d to smartdashboard
+    }
+  }
+
 }
