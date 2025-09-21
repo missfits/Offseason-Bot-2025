b768cf7c0d2b211d3f116517ec9ee6d4aef3f83b
ariakrishnamoorthy <aria.r.krishnamoorthy@gmail.com>
2025-09-21 14:08:16 -0700

    add new constants and remove unused variables

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,7 +12,6 @@ import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.math.util.Units;
-import frc.robot.RobotContainer.RobotName;
 import frc.robot.generated.TunerConstants;
 
 public class Constants {
@@ -58,13 +57,15 @@ public class Constants {
     public static final String CAMERA1_NAME = "beam_camera";  
     public static final String CAMERA2_NAME = "swerve_camera";  
 
-    public static final double ROBOT_TO_CAM1_X = RobotContainer.name == RobotName.CERIDWEN ? Units.inchesToMeters(-2) : 0.31115 ; // in meters from center of robot 
-    public static final double ROBOT_TO_CAM1_Y = RobotContainer.name == RobotName.CERIDWEN ? Units.inchesToMeters(-1) : -0.0508; // in meters from center of robot 
-    public static final double ROBOT_TO_CAM1_Z = RobotContainer.name == RobotName.CERIDWEN ? Units.inchesToMeters(17) : 0.1397; // in meters from the floor?
+
+    // all of these values are from dynamene and need to be fine tuned
+    public static final double ROBOT_TO_CAM1_X = Units.inchesToMeters(-2); // in meters from center of robot 
+    public static final double ROBOT_TO_CAM1_Y = Units.inchesToMeters(-1); // in meters from center of robot 
+    public static final double ROBOT_TO_CAM1_Z = Units.inchesToMeters(17); // in meters from the floor?
     
-    public static final double ROBOT_TO_CAM2_X = RobotContainer.name == RobotName.CERIDWEN ? Units.inchesToMeters(13-4.75) : 0 ; // in meters from center of robot 
-    public static final double ROBOT_TO_CAM2_Y = RobotContainer.name == RobotName.CERIDWEN ? Units.inchesToMeters(13-3.125) : 0; // in meters from center of robot 
-    public static final double ROBOT_TO_CAM2_Z = RobotContainer.name == RobotName.CERIDWEN ? Units.inchesToMeters(7.5) : 0; // in meters from the floor?
+    public static final double ROBOT_TO_CAM2_X = Units.inchesToMeters(13-4.75); // in meters from center of robot 
+    public static final double ROBOT_TO_CAM2_Y = Units.inchesToMeters(13-3.125); // in meters from center of robot 
+    public static final double ROBOT_TO_CAM2_Z = Units.inchesToMeters(7.5); // in meters from the floor?
     
 
     // default vision standard deviation
@@ -78,8 +79,14 @@ public class Constants {
     public static final double MAX_VISION_POSE_PITCH = 0.05; // in radians
     public static final double VISION_DISTANCE_DISCARD = 10; 
 
-    public static final double VISION_ALIGNMENT_DISCARD = Units.inchesToMeters(1); // in meters
+    // all of these constants need to be tuned as well
+    public static final double ROBOT_TO_CAM1_ROLL = 0;
+    public static final double ROBOT_TO_CAM1_YAW = 0;
+    public static final double ROBOT_TO_CAM1_PITCH = 0;
 
+    public static final double ROBOT_TO_CAM2_ROLL = 0;
+    public static final double ROBOT_TO_CAM2_YAW = 0;
+    public static final double ROBOT_TO_CAM2_PITCH = -Math.PI/9;
 
     public static final Translation2d ROBOT_TO_CAM1 = 
       new Translation2d(ROBOT_TO_CAM1_X, ROBOT_TO_CAM1_Y); // in meters from center of robot to 2x4 camera mount
@@ -92,12 +99,6 @@ public class Constants {
     
     public static final Transform3d ROBOT_TO_CAM2_3D = 
       new Transform3d(new Translation3d(ROBOT_TO_CAM2_X, ROBOT_TO_CAM2_Y, ROBOT_TO_CAM2_Z), new Rotation3d(0,0,-Math.PI/9)); // in meters from center of robot to 2x4 camera mount
-  
-
-    public static final double CAMERA_HEIGHT = 0.0951738; // in meters from floor to camera center
-    public static final double CAMERA_PITCH = 0; // in radians, bogus
-    public static final double TARGET_HEIGHT = 0.3048; // in meters to the middle of the apriltag on reef
-    public static final double TARGET_PITCH = 0;
 
     public static final double MAX_AVG_DIST_BETWEEN_LAST_EST_POSES = 0.3; // in meters 
     public static final double MAX_AVG_SPEED_BETWEEN_LAST_EST_POSES = MAX_AVG_DIST_BETWEEN_LAST_EST_POSES * 50.;
