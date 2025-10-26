0db108426ee6c90917ef3f1e718916b204f5c8f5
riley <rileykjen@gmail.com>
2025-10-26 13:58:15 -0700

    remove comment

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -20,7 +20,6 @@ public class Robot extends TimedRobot {
   @Override
   public void robotPeriodic() {
     CommandScheduler.getInstance().run(); 
-    //m_robotContainer.updatePoseEst();
   }
 
   @Override
